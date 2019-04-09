77bb972227545f5cb95af94bc80030143ac115f3
David Moody <dmmoody89@gamil.com>
2019-04-09 11:23:15 -0500

    initial commit with server/router/user

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router=require('./router');
+const app=express();
+
+//app setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type:'*/*' }))
+router(app);
+
+//server setup
+const port=process.env.PORT || 3090;
+const server=http.createServer(app);
+server.listen(port);
+console.log('Server listening on port: ',port)
---
+++ server/router.js
@@ -0,0 +1,6 @@
+module.exports = function(app) {
+    app.get('/',function(req, res, next){
+        res.send(['water','phone','paper'])
+
+    });
+}
