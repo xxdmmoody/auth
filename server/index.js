e6e59460cc0dee4283499b356b3ecc129dd2f9b1
David Moody <dmmoody89@gamil.com>
2019-04-19 08:41:49 -0500

    index/user

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router=require('./router');
+const mongoose=require('mongoose');
 const app=express();
+
+//db setup
+mongoose.connect('mongodb://localhost:27017/auth');
 
 //app setup
 app.use(morgan('combined'));
@@ -15,4 +19,4 @@ router(app);
 const port=process.env.PORT || 3090;
 const server=http.createServer(app);
 server.listen(port);
-console.log('Server listening on port: ',port)
+console.log('Server listening on port: ',port);
